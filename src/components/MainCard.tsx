94bfd319c4bfc5f91a0367f2a8cf148ca2731a9d
Nikitos4443 <Nikitos4443@gmail.com>
2025-09-09 14:40:22 +0300

    feat: add favourites

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -3,12 +3,40 @@
 import {Button} from "@/components/ui/button";
 import {useAppDispatch, useAppSelector} from "@/lib/redux/hooks";
 import {addToCart, removeFromCart} from "@/lib/redux/slices/cart";
+import {useState} from "react";
 
 function MainCard(props: CardProps) {
-    const { id, title, imageUrl, isFavorite, price } = props
+    const { _id, title, imageUrl, isFavorite, price } = props
 
     const cart = useAppSelector((state) => state.cart.chosenFlowers)
-    const isInCart = cart.some(f => f.id === id)
+    const isInCart = cart.some(f => f._id === _id)
+
+    const [isFavourite, setIsFavourite] = useState<boolean>(() => {
+        const favouritesStr = localStorage.getItem('favourites');
+        if (!favouritesStr) return false;
+
+        try {
+            const favouritesArr: string[] = JSON.parse(favouritesStr);
+            return favouritesArr.includes(_id);
+        } catch {
+            return false;
+        }
+    });
+
+    function toggleFavourites() {
+        const favouritesStr = localStorage.getItem('favourites');
+        let favouritesArr: string[] = favouritesStr ? JSON.parse(favouritesStr) : [];
+
+        if (isFavourite) {
+            favouritesArr = favouritesArr.filter(id => id !== _id);
+            setIsFavourite(false);
+        } else {
+            favouritesArr.push(_id);
+            setIsFavourite(true);
+        }
+
+        localStorage.setItem('favourites', JSON.stringify(favouritesArr));
+    }
 
     const dispatch = useAppDispatch();
 
@@ -21,9 +49,10 @@ function MainCard(props: CardProps) {
             />
             <h3 className="mt-3 mb-2 text-lg font-semibold">{title}</h3>
             <img
-                src={isFavorite ? '/favourite.png' : '/notFavourite.png'}
-                alt={isFavorite ? 'В улюблених' : 'Не в улюблених'}
+                src={isFavourite ? '/favourite.png' : '/notFavourite.png'}
+                alt={isFavourite ? 'В улюблених' : 'Не в улюблених'}
                 className="mx-auto w-6 h-6 mb-3 cursor-pointer"
+                onClick={toggleFavourites}
             />
 
             <p className="text-gray-700 font-medium mb-3">{price} ₴</p>
