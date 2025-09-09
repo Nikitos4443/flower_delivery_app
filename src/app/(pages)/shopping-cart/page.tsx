5920708f03c319e30682d5f8038eb2d89cb3ba99
Nikitos4443 <Nikitos4443@gmail.com>
2025-09-09 11:30:08 +0300

    feat: implement shopping cart page

--- src/app/(pages)/shopping-cart/page.tsx
+++ src/app/(pages)/shopping-cart/page.tsx
@@ -1,9 +1,133 @@
+"use client"
+
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod";
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+import {Button} from "@/components/ui/button";
+import {useAppSelector} from "@/lib/redux/hooks";
+import ShoppingCartCard from "@/components/ShoppingCartCard";
+
+const formSchema = z.object({
+    name: z.string("Name must contain minimum 2 and maximum 50 symbols").min(2).max(50),
+    email: z.email("Incorrect email"),
+    phone: z.string("Incorrect phone number"),
+    address: z.string()
+})
 
 function ShoppingCart() {
+
+    const cart = useAppSelector((state) => state.cart.chosenFlowers);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+            address: ""
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
     return (
-        <div>
-            ShoppingCart
-        </div>
+        <main>
+            <div className="flex gap-5 h-6/7 overflow-hidden">
+                <section className="shopping-cart-section h-fit">
+                    <Form {...form}>
+                        <form id="shopping-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-15">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your name" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your email" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="phone"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Phone</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your phone" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="address"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Address</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your address" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </section>
+
+                <section className="shopping-cart-section overflow-y-auto flex flex-col gap-5">
+                    {cart.length > 0 ? (
+                        <>
+                            {cart.map((item) => (
+                                <ShoppingCartCard key={item.id} {...item} />
+                            ))}
+                        </>
+                    ): (
+                        <div>
+                            Nothing chosen
+                        </div>
+                    )}
+                </section>
+            </div>
+            <div className="flex justify-end ">
+                <div className="flex gap-5 items-center">
+                    <span>Total sum: {cart.reduce((acc, flower) => acc + flower.price * flower.count, 0)}</span>
+                    <Button type="submit" form="shopping-form" disabled={cart.length === 0}>
+                        Submit
+                    </Button>
+                </div>
+            </div>
+        </main>
     );
 }
 
--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -1,13 +1,13 @@
 export const cards = [
-    { id: 1, title: 'Card 1', imageUrl: '/window.svg', isFavorite: true, price: 120 },
-    { id: 2, title: 'Card 2', imageUrl: '/globe.svg', isFavorite: false, price: 80 },
-    { id: 3, title: 'Card 3', imageUrl: '/window.svg', isFavorite: true, price: 150 },
-    { id: 4, title: 'Card 4', imageUrl: '/window.svg', isFavorite: false, price: 60 },
-    { id: 5, title: 'Card 5', imageUrl: '/window.svg', isFavorite: false, price: 95 },
-    { id: 6, title: 'Card 6', imageUrl: '/window.svg', isFavorite: true, price: 200 },
-    { id: 7, title: 'Card 7', imageUrl: '/window.svg', isFavorite: false, price: 50 },
-    { id: 8, title: 'Card 8', imageUrl: '/globe.svg', isFavorite: false, price: 170 },
-    { id: 9, title: 'Card 9', imageUrl: '/globe.svg', isFavorite: false, price: 110 },
-    { id: 10, title: 'Card 10', imageUrl: '/window.svg', isFavorite: false, price: 75 }
+    { id: 1, title: 'Card 1', imageUrl: '/window.svg', isFavorite: true, price: 120, shopId: 1 },
+    { id: 2, title: 'Card 2', imageUrl: '/globe.svg', isFavorite: false, price: 80, shopId: 2 },
+    { id: 3, title: 'Card 3', imageUrl: '/window.svg', isFavorite: true, price: 150, shopId: 3 },
+    { id: 4, title: 'Card 4', imageUrl: '/window.svg', isFavorite: false, price: 60, shopId: 1 },
+    { id: 5, title: 'Card 5', imageUrl: '/window.svg', isFavorite: false, price: 95, shopId: 3 },
+    { id: 6, title: 'Card 6', imageUrl: '/window.svg', isFavorite: true, price: 200, shopId: 2 },
+    { id: 7, title: 'Card 7', imageUrl: '/window.svg', isFavorite: false, price: 50, shopId: 1 },
+    { id: 8, title: 'Card 8', imageUrl: '/globe.svg', isFavorite: false, price: 170, shopId: 5 },
+    { id: 9, title: 'Card 9', imageUrl: '/globe.svg', isFavorite: false, price: 110, shopId: 1 },
+    { id: 10, title: 'Card 10', imageUrl: '/window.svg', isFavorite: false, price: 75, shopId: 4 }
 ];
 
