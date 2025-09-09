cd96a8ceea522af0781c73752d94b0baceac2948
Nikitos4443 <Nikitos4443@gmail.com>
2025-09-09 14:37:08 +0300

    feat: add Flower and Shop entities

---
+++ src/lib/models/Flower.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const flowerSchema = new Schema(
+    {
+        title: String,
+        imageUrl: String,
+        price: Number,
+        count: Number,
+        shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+if (mongoose.models.Flower) {
+    mongoose.deleteModel("Flower");
+}
+
+const Flower = mongoose.model("Flower", flowerSchema);
+
+export default Flower;
+export { flowerSchema };
---
+++ src/lib/models/Shop.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const shopSchema = new Schema(
+    {
+        name: String
+    },
+    { timestamps: true }
+);
+
+const Shop = mongoose.models.Shop || mongoose.model("Shop", shopSchema);
+
+export default Shop;
