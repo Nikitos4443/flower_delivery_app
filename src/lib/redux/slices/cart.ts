1d945ee875cb3d3e07fdc971e83e73f744e06b16
Nikitos4443 <Nikitos4443@gmail.com>
2025-09-09 11:29:27 +0300

    feat: add incrementing and decrementing flowers count

--- src/lib/redux/slices/cart.ts
+++ src/lib/redux/slices/cart.ts
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
+type FlowerStateType = Flower & {count: number}
+
 export interface CartState {
-    chosenFlowers: Flower[];
+    chosenFlowers: FlowerStateType[];
 }
 
 const initialState: CartState = {
@@ -14,7 +16,7 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action: PayloadAction<Flower>) => {
-            state.chosenFlowers.push(action.payload)
+            state.chosenFlowers.push({...action.payload, count: 1})
             localStorage.setItem('cart', JSON.stringify(state.chosenFlowers))
             console.log(state.chosenFlowers)
         },
@@ -24,10 +26,29 @@ export const cartSlice = createSlice({
                 state.chosenFlowers.splice(index, 1)
             }
             localStorage.setItem('cart', JSON.stringify(state.chosenFlowers))
+        },
+        incrementCount: (state, action: PayloadAction<number>) => {
+            const flower = state.chosenFlowers.find(f => f.id === action.payload)
+            if (flower) {
+                flower.count += 1
+            }
+            localStorage.setItem('cart', JSON.stringify(state.chosenFlowers))
+        },
+        decrementCount: (state, action: PayloadAction<number>) => {
+            const flower = state.chosenFlowers.find(f => f.id === action.payload)
+            if (flower) {
+                flower.count -= 1
+
+                if(flower.count === 0){
+                    state.chosenFlowers = state.chosenFlowers.filter(f => f.id !== flower.id)
+                }
+            }
+
+            localStorage.setItem('cart', JSON.stringify(state.chosenFlowers))
         }
     },
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart, incrementCount, decrementCount } = cartSlice.actions
 
 export default cartSlice.reducer
